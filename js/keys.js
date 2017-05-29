55017d7ccc5ad94710e5349d3c30241df7e9089f
Hans-Jakob Brandt <olshan15@student.westerdals.no>
2017-05-29 21:54:55 +0200

    everything works1

--- js/keys.js
+++ js/keys.js
@@ -1,7 +1,7 @@
 $(function () {
   'use strict'
 
-  console.log('now we are ready')
+  console.log('keys.js is loaded')
 
   function playKey (e) {
 	  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`) // select audio element based on data-key
@@ -11,7 +11,7 @@ $(function () {
 	  if (!video || !audio) return // stop the function from running if no sound or video
 	  audio.currentTime = 0 // rewind sound before playing
     video.currentTime = 0
-    $(key).addClass('playing') // add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
+    $(key).addClass('playing')
 	  audio.play() // play sound
     video.play()
 
--- js/scenes.js
+++ js/scenes.js
@@ -1,22 +1,29 @@
 $(function () {
   'use strict'
 
-    function scrollEvent () {
-        var currentScene = 1;
+  console.log('scenes.js is loaded');
 
-        $( window ).scroll(function() {
-            var viewportHeight = $(window).height();
-            var container = $('body');
-            var scrollTo = $('#part'+currentScene);
-            container.animate({
-                scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
-            });
-            currentScene++;
-        });
-    };
+
+
+
+
+
+    // function scrollEvent () {
+    //     var currentScene = 1;
+
+    //     $( window ).scroll(function() {
+    //         var viewportHeight = $(window).height();
+    //         var container = $('body');
+    //         var scrollTo = $('#part'+currentScene);
+    //         container.animate({
+    //             scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+    //         });
+    //         currentScene++;
+    //     });
+    // };
     
-    window.addEventListener("scroll", scrollEvent);
-    scrollEvent();
+    // window.addEventListener("scroll", scrollEvent);
+    // scrollEvent();
 
 });
 
