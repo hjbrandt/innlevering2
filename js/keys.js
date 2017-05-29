5c286bba072d5855d305a998b79e8b81b6afd9a7
Hans-Jakob Brandt <olshan15@student.westerdals.no>
2017-05-29 14:57:55 +0200

    Beautified css & html

--- js/keys.js
+++ js/keys.js
@@ -1,18 +1,25 @@
-function playKey(e) {
-  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`); //select audio element based on data-key
-  const key = document.querySelector(`.key[data-key='${e.keyCode}']`); //select key based on key class and data-key
-  if (!audio) return; //stop the function from running if no sound
-  audio.currentTime = 0; //rewind sound before playing
-  audio.play(); //play sound
-  key.classList.add('playing'); //add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
-}
+$(function(){
+"use strict";
 
-function removeKey(e) {
-  if(e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
+	console.log("now we are ready");
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeKey));
+	function playKey(e) {
+	  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`); //select audio element based on data-key
+	  const key = document.querySelector(`.key[data-key='${e.keyCode}']`); //select key based on key class and data-key
+	  if (!audio) return; //stop the function from running if no sound
+	  audio.currentTime = 0; //rewind sound before playing
+	  audio.play(); //play sound
+	  key.classList.add('playing'); //add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
+	}
 
-window.addEventListener ('keydown', playKey);
+	function removeKey(e) {
+	  if(e.propertyName !== 'transform') return;
+	  this.classList.remove('playing');
+	}
+
+	const keys = document.querySelectorAll('.key');
+	keys.forEach(key => key.addEventListener('transitionend', removeKey));
+
+	window.addEventListener ('keydown', playKey);
+
+});
