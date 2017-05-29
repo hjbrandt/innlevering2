fcaed8227de05ef70b643dd877e89dc77b6a3ac3
Nikolai N <nernik15@student.westerdals.no>
2017-05-29 17:50:22 +0200

    Scenes forsøk fra Niko

--- js/scenes.js
+++ js/scenes.js
@@ -1,10 +1,24 @@
 $(function () {
   'use strict'
-  
-  var playAmbience = function() {
-      var amSound = $("#amSound");
-      amSound.play();
-  }();
-  
-  playBackground();
+
+    function scrollEvent () {
+        var currentScene = 1;
+
+        $( window ).scroll(function() {
+            var viewportHeight = $(window).height();
+            var container = $('body');
+            var scrollTo = $('#part'+currentScene);
+            container.animate({
+                scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+            });
+            currentScene++;
+        });
+    };
+    
+    window.addEventListener("scroll", scrollEvent);
+    scrollEvent();
+
 });
+
+
+
