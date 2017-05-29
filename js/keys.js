c0fc0b51c22981ae70baabe63e9cfbaa1d2de89b
Nikolai N <nernik15@student.westerdals.no>
2017-05-29 16:33:18 +0200

    Convert to video element and webM

--- js/keys.js
+++ js/keys.js
@@ -6,10 +6,14 @@ $(function(){
 	function playKey(e) {
 	  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`); //select audio element based on data-key
 	  const key = document.querySelector(`.key[data-key='${e.keyCode}']`); //select key based on key class and data-key
-	  if (!audio) return; //stop the function from running if no sound
-	  audio.currentTime = 0; //rewind sound before playing
+        const video = document.querySelector(`video[data-key='${e.keyCode}']`);
+
+	  if (!video || !audio) return; //stop the function from running if no sound or video
+	  audio.currentTime= 0; //rewind sound before playing
+		video.currentTime = 0;
 		$(key).addClass('playing'); //add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
 	  audio.play(); //play sound
+		video.play();
 
 		audio.onended = function() {
             $(key).removeClass('playing');
