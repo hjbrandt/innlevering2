d5126686b39f919bdeefc5f6025bc8edd2f45df8
Nikolai N <nernik15@student.westerdals.no>
2017-05-29 15:45:59 +0200

    Fixed sound ending function

--- js/keys.js
+++ js/keys.js
@@ -8,13 +8,17 @@ $(function(){
 	  const key = document.querySelector(`.key[data-key='${e.keyCode}']`); //select key based on key class and data-key
 	  if (!audio) return; //stop the function from running if no sound
 	  audio.currentTime = 0; //rewind sound before playing
+		key.classList.add('playing'); //add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
 	  audio.play(); //play sound
-	  key.classList.add('playing'); //add 'playing' to class TODO skriv om til jquery - key.addClass('playing')
+
+		audio.onended = function() {
+			key.classList.remove('playing');
+		};
+
 	}
 
 	function removeKey(e) {
 	  if(e.propertyName !== 'transform') return;
-	  this.classList.remove('playing');
 	}
 
 	const keys = document.querySelectorAll('.key');
