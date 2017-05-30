42fd9332391098b5af7d0c73ca2c1df3d31b344e
Hans-Jakob Brandt <olshan15@student.westerdals.no>
2017-05-30 20:35:36 +0200

    more cleaning

--- js/gui.js
+++ js/gui.js
@@ -3,42 +3,38 @@ $(function() {
   console.log('gui.js is loaded');
 
   // CLOCK
-  const secondHand = document.querySelector('.second-hand'); // TODO skriv om til jquery?
-  const minuteHand = document.querySelector('.minutes-hand'); // TODO skriv om til jquery?
-  const hourHand = document.querySelector('.hour-hand'); // TODO skriv om til jquery?
+  var secondHand = document.querySelector('.second-hand');
+  var minuteHand = document.querySelector('.minutes-hand');
+  var hourHand = document.querySelector('.hour-hand');
 
   function setDate() {
-    const now = new Date();
+    var now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    var seconds = now.getSeconds();
+    var secondsDegrees = ((seconds / 60) * 360) + 90;
 
-    const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    var minutes = now.getMinutes();
+    var minutesDegrees = ((minutes / 60) * 360) + 90;
 
-    const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + 90;
+    var hours = now.getHours();
+    var hoursDegrees = ((hours / 12) * 360) + 90;
 
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // TODO skriv om til jquery?
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`; // TODO skriv om til jquery?
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`; // TODO skriv om til jquery?
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   }
   setInterval(setDate, 1000);
   // END CLOCK
 
   // Mouse move function
-  const inputs = document.querySelectorAll('#overlay');
-
-  function mouseMove() {
-    console.log(this.value);
-  }
+  var inputs = document.querySelectorAll('body');
 
   // Play key and show animation
   function playKey(e) {
 
-    const key = document.querySelector(`.key[data-key='${e.keyCode}']`); // select key based on key class and data-key
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`); // select audio element based on data-key
-    const video = document.querySelector(`video[data-key='${e.keyCode}']`); // TODO skriv om til jquery?
+    var key = document.querySelector(`.key[data-key='${e.keyCode}']`); // select key based on key class and data-key
+    var audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    var video = document.querySelector(`video[data-key='${e.keyCode}']`);
 
     if (!video || !audio) return; // stop the function from running if no sound or video on key
     audio.currentTime = 0; // rewind before playing
@@ -58,9 +54,7 @@ $(function() {
   // END Play key
 
   // Event listeners
-  inputs.forEach(input => input.addEventListener('mouse', mouseMove));
-
-  const keys = document.querySelectorAll('.key'); // TODO skriv om til jquery?
+  var keys = document.querySelectorAll('.key');
   keys.forEach(key => key.addEventListener('transitionend', removeKey));
 
   window.addEventListener('keydown', playKey);
