02a8ea3bc0c36001f26af971eff44530f488d672
Hans-Jakob Brandt <olshan15@student.westerdals.no>
2017-06-04 13:35:40 +0200

    nesten ferdig..

--- js/main.js
+++ js/main.js
@@ -6,17 +6,7 @@ $(function() {
   var newPosition = 0;
   var increment = 1;
 
-  // SCROLL
-
-  function moveUp() {
-    $('.gui');
-    console.log("animate UP here");
-  }
-
-  function moveDown() {
-    $('.gui');
-    console.log("animate UP here");
-  }
+  // SCROLL FUNCTIONS
 
   var scroll = function() {
 
@@ -31,7 +21,7 @@ $(function() {
       }
 
       if ($(window).scrollTop() === 0) {
-        window.scrollTo(0, document.body.scrollHeight - 1);
+        window.scrollTo(0, $(document).height()); //TODO fix looping bug
       }
 
       if (total == 1000) {
@@ -52,11 +42,20 @@ $(function() {
 
       }
 
-    });
+      function moveUp() {
+        $('.gui');
+        console.log("animate UP here");
+      }
 
+      function moveDown() {
+        $('.gui');
+        console.log("animate DOWN here");
+      }
+
+    });
   }(); // END SCROLL
 
-  // KEY
+  // KEY FUNCTIONS
   $('body').on('keydown', function(e) {
 
     // KEY PRESS
@@ -65,6 +64,32 @@ $(function() {
         return;
       e.target.classList.remove('active');
     }
+
+
+    // START getRandonNumber()
+
+      // var getNumber = function(min, max){
+      //     if(min < 0 ){
+      //         min = 0;
+      //     }
+      //     if(max > 100){
+      //         max = 100;
+      //     }
+      //     max = max - min;         
+          
+      //     return (Math.floor(Math.random() * max) + min);
+      // };
+
+    // END getRandonNumber();
+
+    // function addPosition() {
+    //   var X = getNumber();
+    //   var Y = getNumber();
+
+    //   $('.playing').css('left', X);
+    //   console.log("position values");
+    //   $('.playing').css('top', Y);
+    // }();
 
     var press = document.querySelector(`.key-press[data-key='${e.keyCode}']`);
 
@@ -126,25 +151,37 @@ $(function() {
   // GET SCENE from scroll value
   function getScene() {
 
+    var $gui = $('#gui');
+    $gui.find('.gui');
+
+    // scene change breakpoints
     if (total == 200) {
+      $gui.hide();
       $('#scene').removeClass("scene-1").addClass("scene-2");
       SCENE_APP.init();
+      $gui.fadeIn('slow');
     }
     if (total == 400) {
+      $gui.hide();
       $('#scene').removeClass("scene-2").addClass("scene-3");
       SCENE_APP.init();
+      $gui.fadeIn('slow');
     }
     if (total == 600) {
+      $gui.hide();
       $('#scene').removeClass("scene-3").addClass("scene-4");
       SCENE_APP.init();
+      $gui.fadeIn('slow');
     }
     if (total == 800) {
+      $gui.hide();
       $('#scene').removeClass("scene-4").addClass("scene-1");
       SCENE_APP.init();
+      $gui.fadeIn('slow');
       total = 0;
-    }
+    } // END scene change breakpoint
 
-  } // END getScene
+  } // END --getScene()
 
   // START Gui controls
   $('.controls input').on('mousemove', function(e) {
@@ -154,12 +191,60 @@ $(function() {
     $('.gui').css({'opacity': this.value + data });
   });
 
-  $(function() {
-    $('#slider').slider();
-  });
-    slider.slider({
-      range: "min",
-      valaue: 50,
-    });
+  // // START js UI
+  // $(function() {
+  //   $('#slider').slider();
+  //   slider.slider({
+  //     range: "min",
+  //     valaue: 50,
+  // });
+  //   });
+
+    // $(function() {
+     
+    //   var slider = $('#slider'),
+    //     tooltip = $('.tooltip');
+     
+    //   tooltip.show();
+     
+    //   slider.slider({
+    //     range: "min",
+    //     min: 1,
+    //     value: 35,
+     
+    //     start: function(event,ui) {
+    //       tooltip.fadeIn('fast');
+    //     },
+     
+    //     slide: function(event, ui) {
+     
+    //       var value = slider.slider('value'),
+    //         volume = $('.volume');
+     
+    //       tooltip.css('left', value).text(ui.value);
+     
+    //       if(value <= 5) { 
+    //         volume.css('background-position', '0 0');
+    //       } 
+    //       else if (value <= 25) {
+    //         volume.css('background-position', '0 -25px');
+    //       } 
+    //       else if (value <= 75) {
+    //         volume.css('background-position', '0 -50px');
+    //       } 
+    //       else {
+    //         volume.css('background-position', '0 -75px');
+    //       };
+     
+    //     },
+     
+    //     stop: function(event,ui) {
+    //       tooltip.fadeOut('fast');
+    //     },
+    //   });
+     
+    // });
+
+
 
 }());
